9473a5e652996510c39ed28ab92de4794baac1d3
Alice Debarge <alicedbrg@gmail.com>
2018-08-21 14:15:13 +0200

    Add/Remove selected cell

--- js/script.js
+++ js/script.js
@@ -10,9 +10,63 @@ function checkArray (myArray, myString) {
     }
 };
 
-var clickedCell = '';
-$(".fruits-grid td").click(function () {
-    clickedCell += $(this)[0].innerHTML;
-    console.log(clickedCell);
 
+
+var clickedCell = [];
+
+$(".fruits-grid td").click(function () {
+    if ($(this).hasClass("selected")) {
+        $(this).removeClass("selected");
+        clickedCell.pop(this.innerHTML);
+        // $(this).html()
+    }
+    else {
+        $(this).addClass("selected");
+        clickedCell.push(this.innerHTML);
+    }
+    var word = clickedCell.join("");
+    console.log(word);
 });
+
+
+
+
+
+
+// var currentWord = '';
+// var foundWord = '';
+
+// currentWord = 'C';
+// currentWord.length = 1;
+
+// currentWord = 'CO';
+// currentWord.length = 2;
+
+
+// ONCLICK FUNCTION
+fruits.forEach(function (word) {
+    currentWord = "";
+    var clickedLetter = "";
+    if(clickedLetter === word[currentWord.length]) {
+        currentWord += maLettreClickée;
+        if (fruits.includes(currentWord)) {
+             foundWord = currentWord;
+             return foundWord;
+            }  
+        }
+})
+
+// ($(this).prev().hasClass("one")) || $(this).next().hasClass("one")
+// $("td.one").next();
+
+
+
+// currentWord = 'COC';
+// currentWord.length = 3;
+
+// // ONCLICK FUNCTION
+// possibleWords.forEach( function (word) {
+//     if (clickedLetter === word [currentWord.length]) {
+//         currentWord += maLettreClickée;
+//     }
+// })
